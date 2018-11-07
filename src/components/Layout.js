146ba84752e7596fe5bc53a9ecab7a188f01300f
gscottqueen <geoffreysqueen@gmail.com>
2018-11-07 15:47:43 -0500

    updated helmet title

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Inaura Studios | Digital Zen" />
     {/* <Navbar /> */}
     <div>{children}</div>
   </div>
