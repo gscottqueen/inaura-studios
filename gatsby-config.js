921a19f27e2b5977d6485bfbd6ca9db9b7040ae3
gscottqueen <geoffreysqueen@gmail.com>
2018-11-07 15:43:55 -0500

    updated title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Inaura Studios | Websites & Applications',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
