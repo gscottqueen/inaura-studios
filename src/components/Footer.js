9fa5183acd9944cde788ecbff1a5852ae17f35b0
gscottqueen <geoffreysqueen@gmail.com>
2018-11-08 15:30:41 -0500

    new footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './footer.css'
+
+
+const Footer = () => (
+  <footer>
+    <div>
+      <p><a href="mailto:info@inaurastudios.com">info@inaurastudios.com</a></p>
+      <p>Located in Washington, DC</p>
+    </div>
+  </footer>
+)
+
+export default Footer
