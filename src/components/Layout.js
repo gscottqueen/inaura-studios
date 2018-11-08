cd94d9ac95f48f1c99b9b9a0a983307a680eefc8
gscottqueen <geoffreysqueen@gmail.com>
2018-11-08 15:30:53 -0500

    updated main layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-// import Navbar from '../components/Navbar'
-import './all.sass'
+import Footer from '../components/Footer'
 import '../fonts/fonts.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Inaura Studios | Digital Zen" />
-    {/* <Navbar /> */}
-    <div>{children}</div>
+    <main>{children}</main>
+    <Footer />
   </div>
 )
 
