c9142cc272f0579066fd4a4b5eb3924bcba1a37e
gscottqueen <geoffreysqueen@gmail.com>
2018-11-09 13:18:11 -0500

    including a favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,18 +3,23 @@ import Helmet from 'react-helmet'
 
 import Footer from '../components/Footer'
 import '../fonts/fonts.css'
+import favicon from '../img/favicon.ico'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet 
       title="Inaura Studios | Your Digital Zen" 
       meta={[
-        { name: 'description', content: 'We craft custom fidelity prototypes for big digital ideas.'},
+        { name: 'description', content: 'Looking to build an app? We craft custom fidelity prototypes for big digital ideas.'},
         { name: 'keywords', content: 'prototypes , digital' },
         { property:'og:description', content: 'We craft custom fidelity prototypes for big digital ideas.' },
         { property: 'og:url', content: '//inaurastudios.com/'},
         { property: 'twitter:description', content: 'We craft custom fidelity prototypes for big digital ideas.'},
         { property: 'twitter:url', content: '//inaurastudios.com/'},
+      ]}  
+      link={[
+      { rel: 'shortcut icon', type: 'image/ico', href: `${favicon}` },
+      {rel: "canonical", href: "//inaurastudios.com"} 
       ]}/>
     <main>{children}</main>
     <Footer />
