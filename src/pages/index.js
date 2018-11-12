8fcae7c47e3956f0d7591578504604889b5cfb6f
gscottqueen <geoffreysqueen@gmail.com>
2018-11-12 16:10:48 -0500

    updated styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,10 @@ export default class IndexPage extends React.Component {
 
     return (
       <Layout>
-        <section>
-          <h1>Inaura<br></br> Studios</h1>
-          <h2>Your digital zen.</h2>
-          <p>We craft custom fidelity prototypes for big digital ideas.</p>
+        <section className="landing">
+          <h1 className="title">Inaura<br></br> Studios</h1>
+          <h2 className="sub-title">Your digital zen.</h2>
+          <p className="description">We craft custom fidelity prototypes for big digital ideas.</p>
         </section>
         <div className="block-anchor"></div>
       </Layout>
