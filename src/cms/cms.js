f35cda304c870e82316829d9b5586fe37ed7ac7c
gscottqueen <geoffreysqueen@gmail.com>
2018-11-15 09:33:43 -0500

    importing section preview template to register in cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,10 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import SectionPreview from './preview-templates/SectionPreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('section', SectionPreview)
+
